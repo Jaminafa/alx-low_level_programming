1e98cd4e30b81b79674df41ff3a118526bfb91a3
Jaminafa <jaminafa@gmail.com>
2022-12-08 17:18:43 -0800

    Create script 100-intel

---
+++ 0x00-hello_world/x.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+/**
+  * main - This program prints the size of various types on the
+  * computer it is compiled and run on.
+  * Return: This rturns 0
+  */
+
+int main(void)
+{
+	int x;
+
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	return (0);
+}
