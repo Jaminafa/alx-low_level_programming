d66cfaca67f459fcde432bccf1d261d34334e499
Jaminafa <jaminafa@gmail.com>
2022-12-11 15:49:18 -0800

    Task completed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - Entry point
+  * Description: Prints alphabet in reverse
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
