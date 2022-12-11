eecfe4ee791d9afa5e741e5458a70d80ec069375
Jaminafa <jaminafa@gmail.com>
2022-12-11 14:42:35 -0800

    Update task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,19 +3,18 @@
 #include <stdio.h>
 
 /**
-  * main - This program identifies if a randomly generated number
+  * main - Entry point
+  * Description: This program identifies if a randomly generated number
   * is greater thatn 5, is zero or is less thatn 6
   * and not 0
   * Return: This returns 0
   */
 int main(void)
 {
-	int n;
+	int n, x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	int x;
 
 	x = n % 10;
 
