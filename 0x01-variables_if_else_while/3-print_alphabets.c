fcb4d2e5796c68577000989ddcbd01330e273869
Jaminafa <jaminafa@gmail.com>
2022-12-11 15:10:35 -0800

    Task completed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - Entry point
+  * Description: Prints alphabet in lowercase then uppercase
+  * Returb=n: Always 0
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x)
+	}
+	putchar(\n)
+
+	return (0)
+
+}
