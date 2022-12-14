a1adccbade7b67b17bce33e26432ce3d79700408
Jaminafa <jaminafa@gmail.com>
2022-12-13 16:02:29 -0800

    Task completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - Entry point
+  * Description: afunction that prints the alphabet in
+  * lowercase followed by a new line
+  * Return: Always 0
+  */
+  int main(void)
+{
+	char letter + 'a';
+
+	while (lettr <= 'z');
+	{
+		_putchar(letter);
+		letter++;
+
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project aims to introduce the nested loop staements in C programming
