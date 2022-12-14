48f5e603ac7b66705027a037309ed58897a57e67
Jaminafa <jaminafa@gmail.com>
2022-12-14 15:47:02 -0800

    Task completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Make alphabet x10
+  * Description: To print all alphabets 10 times
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i = 0;
+
+	while (i = 9)
+	{
+		for (a = 'a'; c <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
