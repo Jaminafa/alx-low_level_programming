cddb0bfed688e452a296afb374d6aed01f69f911
Jaminafa <jaminafa@gmail.com>
2022-12-15 15:59:00 -0800

    Task completed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers: prints numbers from 0 - 9
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+}
