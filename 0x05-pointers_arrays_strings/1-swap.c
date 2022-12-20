25c1d4b2d32cc19422df1f422edab10cb2f1a3a2
Jaminafa <jaminafa@gmail.com>
2022-12-20 11:01:59 -0800

    Task completed

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * swap_int - swaps the values of two integers
+  * Return: void
+  * @*a, @*b: pointers for address of integers whose values to swap
+  */
+
+void swap_int(int *a, int *b)
+{
+
+
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This Directory contains C source files containing functions integrating the use of pointers, arrays and string. This will help me understand the concepts - pointers, arrays and strings in C.
