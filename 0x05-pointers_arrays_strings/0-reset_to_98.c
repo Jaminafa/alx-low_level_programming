afcef2fabc4c04e4ce6cf7c3a8c119b05597b08e
Jaminafa <jaminafa@gmail.com>
2022-12-20 11:14:24 -0800

    Task completed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
-  * reset_to_98: resets the value of a variable to 98
-  * Return: void
-  */
+ * reset_to_98 - Resets variable to 98
+ * @n:  pointer for storing address of variable
+ *
+ * Return: void
+ */
 
 void reset_to_98(int *n)
 {
