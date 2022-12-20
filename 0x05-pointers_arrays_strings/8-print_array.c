47df89f87aa586dc3ba62ad72c41da3f3c459617
Jaminafa <jaminafa@gmail.com>
2022-12-20 14:47:42 -0800

    Task completed

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_array - print 'n' elements of an array of integers
+  * @a: int type array pointer
+  * @n: int type integer
+  * Description: Number must be separated by comma and space,
+  *
+  * Return: void
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *s);
 void puts_half(char *s);
+void print_array(int *a, int n);
