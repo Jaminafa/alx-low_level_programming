ba08970c299f725970c339799cb677ca27dd89cc
Jaminafa <jaminafa@gmail.com>
2023-01-09 05:39:03 -0800

    Task Updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -15,12 +15,15 @@
 int main(int argc, char *argv[])
 {
 	int i;
-
-	for (i = 0; *argv; i++, argv++)
-		;
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
 
 	printf("%d\n", i - 1);
-
+	}
 
 	return (0);
 }
