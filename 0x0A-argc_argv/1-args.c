fc0708757256f64e64ea3a28dd9152c01d2e475b
Jaminafa <jaminafa@gmail.com>
2023-01-09 04:45:28 -0800

    Create source code file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - entry point.
+  * Description: program prints the number of arguments 
+  * passed to it.
+  * @argc: This arguement counts the number of
+  * arguements pased to the program.
+  * @argv: this is a string that stores the
+  * arguements passed to the prgram.
+  * Return: 0 if successful.
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
