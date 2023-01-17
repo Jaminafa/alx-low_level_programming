beb3c8f8cff81973c9363c9a0191d0a784590025
Jaminafa <jaminafa@gmail.com>
2023-01-16 22:53:55 -0800

    Upate dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,17 +20,9 @@ struct dog
 };
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * dog_t - Typedef for struct dog
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
