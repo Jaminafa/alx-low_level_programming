968e26cf1f26b5fd73b0da5cb8b6736ceb069c67
Jaminafa <jaminafa@gmail.com>
2023-01-31 08:33:13 -0800

    Update 0-strcat.c source file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
-#inlcude "main.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
   * _strcat - concatenates two strings
-  * @dest - destination string
-  * @src - source string
-  * Return - concatenated string
+  * @*dest: destination string
+  * @*src: source string
+  * Return: concatenated string
   */
 
 char *_strcat(char *dest, char *src)
@@ -22,7 +23,7 @@ char *_strcat(char *dest, char *src)
 	{
 		;
 	}
-	for (x = 0; src[x] !='\0'; || x < i; x++, j++)
+	for (x = 0; src[x] != '\0'; || x < i; x++, j++)
 	{
 		dest[j] = src[x];
 	}
