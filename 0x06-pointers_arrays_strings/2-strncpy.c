c51deb9e09f4786cd8432ef49ba49c0a3dede05a
Jaminafa <jaminafa@gmail.com>
2023-02-13 05:49:19 -0800

    Create source file 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,23 @@
-#incllude "main.h"
+#include "main.h"
 
-char *strncpy(char *dest, char *src, int n)
+/**
+  * *_strncpy - Function copies a string from one
+  * source to another.
+  * @dest: Destination string
+  * @src: Source string
+  * @n: number of bytes to copy
+  *
+  * Return: Return destination string
+  */
+
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+	int x;
 
+	for (i = 0, x = 0; src[x] && i < n; i++, x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
 }
