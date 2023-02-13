629a075bddb71a6ff13824aa1db2070624dd28a6
Jaminafa <jaminafa@gmail.com>
2023-02-13 06:19:26 -0800

    Update source file 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
 	int i;
+	int j = 0;
 
 	for (i = 0, x = 0; src[x] && i < n; i++, x++)
 	{
 		dest[x] = src[x];
+	}
+
+	for (i = 0; src[i]; i++)
+		j++;
+
+	for (i = j; dest[i] && i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
