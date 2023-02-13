bc57747d24dac170fc34579f39b8cc8423847105
Jaminafa <jaminafa@gmail.com>
2023-02-13 05:07:52 -0800

    Create source file 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
+/**
+  * *_strncat - concatenates two strings
+  * @dest: destination string
+  * @src: source string
+  * @n: number of bytes to concatenate
+  * Return: concatenated string
+  */
+
 char *_strncat(char *dest, char *src, int n)
 {
+	int j;
+	int x;
 
+	for (j = 0; dest[j]; j++)
+	{
+		;
+	}
+	for (x = 0; src[x] &&x < n; x++, j++)
+	{
+		dest[j] = src[x];
+	}
+
+	return (dest);
 }
